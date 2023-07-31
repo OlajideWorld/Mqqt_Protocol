c14d484c6245efe5b18d878894bba8dcb2b57700
OlajideWorld <olajideoken@gmail.com>
2023-07-31 02:24:55 +0100

    mqqt server

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,16 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,61 +33,133 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // final brokerAddress = 'tcp://7.tcp.eu.ngrok.io:13952';
+  final mqttClient =
+      MqttServerClient.withPort("2.tcp.eu.ngrok.io", 'Olajide',18363);
+
   int _counter = 0;
+  final TextEditingController _message = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    connectToBroker();
+  }
+
+  void subscribeToTopic() {
+    const topicToDevice = 'devices/solar_charge_controller';
+
+    final qosLevel = MqttQos.atLeastOnce;
+
+    mqttClient.subscribe(topicToDevice, qosLevel);
+  }
+
+  void onMessageReceived(List<MqttReceivedMessage<MqttMessage>> messages) {
+    messages.forEach((message) {
+      final MqttPublishMessage payload = message.payload as MqttPublishMessage;
+      final data =
+          MqttPublishPayload.bytesToStringAsString(payload.payload.message);
+
+      // Handle the incoming message here based on the topic and data.
+      print(
+          'Received message: ${payload.variableHeader!.topicName}, data: $data');
+    });
+  }
+
+  Future<void> connectToBroker() async {
+    try {
+      await mqttClient.connect();
+      debugPrint('Connected to MQTT broker.');
+    } catch (e) {
+      debugPrint('MQTT connection failed: $e');
+    }
+  }
+
+  Future<void> publishToDevices(String message) async {
+    final builder = MqttClientPayloadBuilder();
+    builder.addString(message);
+
+    final topicFromApp = 'app/to_devices';
+    final payload = builder.payload;
+
+    if (payload != null) {
+      mqttClient.publishMessage(topicFromApp, MqttQos.exactlyOnce, payload);
+    } else {
+      print('Payload is null. Message not sent.');
+    }
+  }
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: 60,
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade700,
+                    borderRadius: const BorderRadius.all(Radius.circular(5))),
+                child: TextFormField(
+                  style: TextStyle(color: Colors.white.withOpacity(0.7)),
+                  cursorColor: Colors.white,
+                  controller: _message,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.lock_clock_outlined,
+                        color: Colors.white.withOpacity(0.7),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey.shade700)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey.shade700)),
+                      hintStyle:
+                          TextStyle(color: Colors.white.withOpacity(0.7)),
+                      hintText: 'Enter message'),
+                  validator: (value) {
+                    // You can add more username validation if needed
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(height: 30),
+              GestureDetector(
+                onTap: () => publishToDevices(_message.text.trim()),
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(color: Colors.blue),
+                  child: const Center(
+                    child: Text(
+                      "Send Message",
+                      style: TextStyle(color: Colors.yellow),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
